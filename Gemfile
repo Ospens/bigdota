9a6c32e6eaca4788cf627c07b41ff10416100dfc
Unknown <1ospens1@gmail.com>
2018-07-17 22:30:34 +0300

    123

--- Gemfile
+++ Gemfile
@@ -13,6 +13,9 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'haml'
 gem "coffee-rails"
+gem 'nokogiri'
+gem 'json'
+gem 'mechanize' 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/parse_controller.rb
+++ app/controllers/parse_controller.rb
@@ -1,4 +1,28 @@
 class ParseController < ApplicationController
+  before_action :auth, only: :index
 
-  def index; end
+  def index
+    @html = @agent.get('https://ru.dotabuff.com/esports/teams/1883502-virtus-pro/players?date=year&league_tier=premium')
+                  .search('article.r-tabbed-table')
+                  .search('table').search('tbody').css('tr')
+    @html.each_with_index do |tr, i|
+      tr.css('td').each_with_index do |td, index|
+        if index == 1 
+          p td.search('.esports-player').first.attributes['href'].value
+        else
+          p td["data-value"]
+        end
+      end
+        p i
+    end
+  end
+
+  private
+
+  def auth
+    @agent = Mechanize.new do |agent|
+      agent.user_agent_alias = 'Linux Mozilla'
+      agent.request_headers = { 'X-Requested-With' => 'XMLHttpRequest' }
+    end
+  end
 end
